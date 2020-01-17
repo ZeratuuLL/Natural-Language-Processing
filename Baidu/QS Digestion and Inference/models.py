72cf366ee4bf56b4bd49d83b18002e31f7e1f24a
Lifeng Wei <lfwei@ucdavis.edu>
2020-01-16 19:48:44 -0800

    Add beam search to decoder, changed decoder.forward()

--- Baidu/QS Digestion and Inference/models.py
+++ Baidu/QS Digestion and Inference/models.py
@@ -21,9 +21,11 @@ class pytorch_encoder(nn.Module):
     This encoder assumes that the input is a batch of same-length sentences
     '''
     
-    def __init__(self, vocab_size, embedding_weights=None, gate_type='gru', hidden_size=128, n_layers=2, dropout_rate=0, bidirectional=False, frozen=True):
+    def __init__(self, vocab_size, start, end, embedding_weights=None, gate_type='gru', hidden_size=128, n_layers=2, dropout_rate=0, bidirectional=False, frozen=True):
         '''
         vocab_size : the number of words
+        start : start token
+        end : end token
         embedding_size : the length of words' embedding vectors
         embedding_weights : the pretrained embedding for words with shape (vocab_size, embedding_size), optional
         gate_type : gate type for RNN structure
@@ -35,6 +37,10 @@ class pytorch_encoder(nn.Module):
         '''
         super(pytorch_encoder, self).__init__()
         self.gate_type = gate_type.lower()
+        self.start_token = start
+        self.end_token = end
+        self.hidden_size = hidden_size
+        
         if gate_type.lower() == 'lstm':
             self.rnn_cell = nn.LSTM(input_size=hidden_size, hidden_size=hidden_size, num_layers=n_layers, bidirectional=bidirectional, dropout = dropout_rate, batch_first=True)
         elif gate_type.lower() == 'gru':
@@ -69,6 +75,12 @@ class pytorch_encoder(nn.Module):
             output, h = self.rnn_cell(embedded)
         return output, h
     
+    def update_tokens(self, start, end):
+        '''
+        Update the start and end tokens
+        '''
+        self.start_token = start
+        self.end_token = end
 
 class Bahdanau_Attention(nn.Module):
 
@@ -107,9 +119,11 @@ class pytorch_decoder(nn.Module):
     This decoder assumes that the input is a batch of same-length sentences, support of attention will be added later
     '''
     
-    def __init__(self, vocab_size, embedding_weights=None, gate_type='gru', hidden_size=128, frozen=True, attn=None):
+    def __init__(self, vocab_size, start, end, embedding_weights=None, gate_type='gru', hidden_size=128, frozen=True, attn=None):
         '''
         vocab_size : the number of words
+        start : start token
+        end : end token
         embedding_size : the length of words' embedding vectors
         embedding_weights : the pretrained embedding for words with shape (vocab_size, embedding_size), optional
         gate_type : gate type for RNN structure
@@ -118,13 +132,17 @@ class pytorch_decoder(nn.Module):
         attn : attention module
         '''
         super(pytorch_decoder, self).__init__()
+        self.start_token = start
+        self.end_token = end
+        self.hidden_size = hidden_size
+        self.gate_type = gate_type
+        
         self.output = nn.Linear(hidden_size, vocab_size)
         if attn is not None:
             self.attn_combine = nn.Linear(attn.input_size + hidden_size, hidden_size)
-        self.softmax = nn.LogSoftmax(dim=-1)
-        self.gate_type = gate_type
+        self.logsoftmax = nn.LogSoftmax(dim=-1)
         self.attn = attn
-        self.hidden_size = hidden_size
+        
         if gate_type.lower() == 'lstm':
             self.rnn_cell = nn.LSTM(input_size=hidden_size, hidden_size=hidden_size, batch_first=True)
         elif gate_type.lower() == 'gru':
@@ -150,10 +168,12 @@ class pytorch_decoder(nn.Module):
         
         Outputs:
         ========
-        output : size (bs, 1, vocab_size) as the probability for each word here
+        output : size (bs, 1, vocab_size) as the log probability for each word here
         hidden : size (1, bs, hidden_size) as the hidden cell of this step
-        cell : size (1, bs, hidden_size) as the cell info of this steo if using LSTM
+        cell : size (1, bs, hidden_size) as the cell info of this step if using LSTM
         '''
+        if cell is None:
+            cell = torch.zeros(hidden.size()).to(device)
         if embedding is None:
             embedding = self.embedding
         embedded = embedding(input)
@@ -164,13 +184,87 @@ class pytorch_decoder(nn.Module):
             embedded = self.attn_combine(embedded)
         if self.gate_type == 'lstm':
             output, (h, c) = self.rnn_cell(embedded, (hidden, cell))
-            output = self.softmax(self.output(output)).squeeze() # remove the second dim with length 1, which is seq length
-            return output, h, c
+            output = self.logsoftmax(self.output(output)).squeeze() # remove the second dim with length 1, which is seq length
         else:
             output, h = self.rnn_cell(embedded, hidden)
-            output = self.softmax(self.output(output)).squeeze() # remove the second dim with length 1, which is seq length
-            return output, h
-       
+            output = self.logsoftmax(self.output(output)).squeeze() # remove the second dim with length 1, which is seq length
+        return output, h, cell
+    
+    def update_tokens(self, start, end):
+        '''
+        Update the start and end tokens
+        '''
+        self.start_token = start
+        self.end_token = end
+        
+    def predict(self, beam, hidden, enc_output, embedding=None, cell=None, length=60):
+        '''
+        Inputs:
+        =======
+        beam : int, the number of beams to search and maintain
+        hidden : size (1, seq_num, hidden_size) as the previous hidden cell (output from previous step)
+        enc_output : size (seq_num, max_len, hidden_size) or (bs, max_len, 2*hidden_size), the output features from the last layer of encoder
+        cell : size (1, seq_num, hidden_size) cell for LSTM if necessary
+        embedding : an embedding layer, if None then self.embedding will be used. 
+                    It can be used to pass an outside embedding layer. 
+                    This is necessary because we cannot use pytorch optimizers if self.embedding.weight.requires_grad = False
+        length : int, indicating the maximum allowed length for prediction
+        
+        Outputs:
+        ========
+        sequences : a list of seq_num sequences with various lengths
+        '''
+        if embedding is None:
+            embedding = self.embedding
+        
+        seq_num = enc_output.size(0)
+        # first step, pick candidates
+        sequences = [[self.start_token.cpu().item()] for _ in range(seq_num)]
+        inputs = torch.tensor([[seq[-1]] for seq in sequences], dtype=torch.long).to(device)
+        cell = torch.zeros((1, seq_num, self.hidden_size)).to(device)
+        log_probs, hidden, cell = self.forward(inputs, hidden, enc_output, embedding, cell)
+        log_probs = log_probs.squeeze() # (seq_num, vocab_size)
+        log_probs, largest_tokens = log_probs.topk(k=beam, dim=-1)
+        largest_tokens = largest_tokens.view(-1, 1) # (beam * seq_num, 1)
+        log_probs = log_probs.view(-1, 1) # (beam * seq_num, 1), the log_likelihood after first prediction
+        sequences = [[self.start_token.cpu().item(), token.cpu().item()] for token in largest_tokens] # save the first two tokens, every beam lists are the searching results for one sentence
+        
+        # expand to pretend to have beam * seq_num sequences, notice that we combine repeat() and view() here to generate consecutive repeats
+        hidden = hidden.repeat(1, 1, beam).view(1, beam*seq_num, self.hidden_size) # to shape (1, beam * seq_num, hidden_size)
+        enc_output = enc_output.repeat(1, beam, 1).view(beam*seq_num, -1, self.hidden_size) # to shape (beam * seq_num, max_len, hidden) 
+        
+        const_tensor = torch.tensor([[0] + [-int(1e4)]*(beam-1)], dtype=torch.float).to(device) # to add when some sentence has reached an end for beam search, so that only one candidate remains
+        
+        for _ in range(1, length):
+            
+            inputs = torch.tensor([[seq[-1]] for seq in sequences], dtype=torch.long).to(device)
+            isEnd = (inputs==self.end_token).float()
+            if all(inputs == end):
+                break
+            else:
+                step_log_probs, hidden, cell = self.forward(inputs, hidden, enc_output, embedding, cell)
+                step_log_probs, step_largest_token = step_log_probs.topk(k=beam, dim=-1) # ()
+                log_probs = log_probs + (1-isEnd) * step_log_probs + isEnd * const_tensor # (beam * seq_num, beam)
+                log_probs = log_probs.view(seq_num, beam**2)
+                step_largest_token = step_largest_token.view(seq_num, beam**2)
+                log_probs, step_largest_index = log_probs.topk(k=beam, dim=-1)
+                log_probs = log_probs.view(-1, 1)
+                temp = []
+                for i in range(seq_num):
+                    for j in range(beam):
+                        token_index = step_largest_index[i, j].item()
+                        seq_index = i*beam + token_index//beam
+                        sequence = sequences[seq_index][:] # so that this would be a copy
+                        if isEnd[seq_index]:
+                            None
+                        else:
+                            sequence.append(step_largest_token[i, token_index].item())
+                        temp.append(sequence)
+                sequences = temp
+        
+        return sequences
+            
+        
 class Seq2Seq(nn.Module):
     
     def __init__(self, vocab_size, start_token, end_token, embedding_weights=None, gate_type='gru', hidden_size=128, n_layers=2, attention=True, dropout_rate=0, teaching_rate=0.5, bidirectional=False, frozen=True):
@@ -179,13 +273,16 @@ class Seq2Seq(nn.Module):
         self.gate_type = gate_type
         self.teaching_rate = teaching_rate
         self.bidirectional = bidirectional
+        self.start_token = start_token.to(device)
+        self.end_token = end_token.to(device)
+        self.teaching_rate = teaching_rate
         
-        self.encoder = pytorch_encoder(vocab_size, None, gate_type, hidden_size, n_layers, dropout_rate, bidirectional, False).to(device)
+        self.encoder = pytorch_encoder(vocab_size, self.start_token, self.end_token, None, gate_type, hidden_size, n_layers, dropout_rate, bidirectional, False).to(device)
         if attention:
             self.attention = Bahdanau_Attention(hidden_size, bidirectional).to(device)
         else:
             self.attention = None
-        self.decoder = pytorch_decoder(vocab_size, None, gate_type, hidden_size, False, self.attention).to(device)
+        self.decoder = pytorch_decoder(vocab_size, self.start_token, self.end_token, None, gate_type, hidden_size, False, self.attention).to(device)
         if embedding_weights is not None:
             self.embedding = nn.Embedding(vocab_size, hidden_size).to(device)
             self.embedding.weight = nn.Parameter(embedding_weights).to(device)
@@ -194,10 +291,6 @@ class Seq2Seq(nn.Module):
             self.embedding = None
             
         self.set_optimizers(0.001)
-        
-        self.start_token = start_token.to(device)
-        self.end_token = end_token.to(device)
-        self.teaching_rate = teaching_rate
         
     def set_optimizers(self, learning_rate):
         self.embedding_optimizer = None
@@ -275,10 +368,7 @@ class Seq2Seq(nn.Module):
                 
                 for index in range(1, y.size(1)-1):
                     dec_input = y[:, [index]]
-                    if self.gate_type == 'lstm':
-                        dec_output, dec_hidden, cell = self.decoder(dec_input, dec_hidden, enc_output, common_embedding, cell)
-                    else:
-                        dec_output, dec_hidden = self.decoder(dec_input, dec_hidden, enc_output, common_embedding)
+                    dec_output, dec_hidden, cell = self.decoder(dec_input, dec_hidden, enc_output, common_embedding, cell)
                     losses = criterion(dec_output, y[:, index+1])
                     loss += (notEnd * losses).sum()
                     
@@ -330,7 +420,7 @@ if __name__ == '__main__':
     
     # embedding, encoder, decoder
     embedding_weights = torch.FloatTensor(vocab_size, hidden_size).uniform_(-1, 1)
-    test_enc = pytorch_encoder(vocab_size, embedding_weights, gate_type, hidden_size, n_layers, 0, bidirectional)
+    test_enc = pytorch_encoder(vocab_size, 0, vocab_size-1, embedding_weights, gate_type, hidden_size, n_layers, 0, bidirectional)
     
     # test encoder
     enc_output, enc_hidden = test_enc(test_inputs)
@@ -351,7 +441,7 @@ if __name__ == '__main__':
     print('hidden vector after attention size : {}\n'.format(real_hidden.size()))
     
     # test decoder without attention
-    test_dec = pytorch_decoder(vocab_size, embedding_weights, gate_type, hidden_size)
+    test_dec = pytorch_decoder(vocab_size, 0, vocab_size-1, embedding_weights, gate_type, hidden_size)
     print('Decoding start\n')
     dec_hidden = enc_hidden[[-1], :, :]
     test_output_start = [random.sample(all_words, 1) for _ in range(sentence_number)]
@@ -360,10 +450,7 @@ if __name__ == '__main__':
     decoded[: , [0]] = torch.tensor(test_output_start, dtype=torch.long)
     cell = torch.FloatTensor(1, sentence_number, hidden_size).normal_(0, 2)
     for i in range(1, output_length):
-        if gate_type=='lstm':
-            dec_output, dec_hidden, cell = test_dec(dec_output, dec_hidden, enc_output, cell)
-        else:
-            dec_output, dec_hidden = test_dec(dec_output, dec_hidden, enc_output)
+        dec_output, dec_hidden, cell = test_dec(dec_output, dec_hidden, enc_output, cell)
         _, dec_output = dec_output.topk(1) # top1 output
         dec_output = dec_output.long()
         decoded[:, [i]] = dec_output
@@ -380,10 +467,7 @@ if __name__ == '__main__':
     decoded[: , [0]] = torch.tensor(test_output_start, dtype=torch.long)
     cell = torch.FloatTensor(1, sentence_number, hidden_size).normal_(0, 2)
     for i in range(1, output_length):
-        if gate_type=='lstm':
-            dec_output, dec_hidden, cell = test_dec(dec_output, dec_hidden, enc_output, cell)
-        else:
-            dec_output, dec_hidden = test_dec(dec_output, dec_hidden, enc_output)
+        dec_output, dec_hidden, cell = test_dec(dec_output, dec_hidden, enc_output, cell)
         _, dec_output = dec_output.topk(1) # top1 output
         dec_output = dec_output.long()
         decoded[:, [i]] = dec_output
